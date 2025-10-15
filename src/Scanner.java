af64eb7f50d39695ebabc7272d3d84cda61e1fb2
Fernanda-SAV <fernanda.sav@discente.ufma.br>
2025-10-15 07:23:23 -0300

    Inclusao da classe scanner

--- src/Scanner.java
+++ src/Scanner.java
@@ -7,4 +7,37 @@ public class Scanner {
         this.input = input;
     }
 
+    private char peek () {
+        if (current < input.length)
+            return (char)input[current];
+        return '\0';
+    }
+
+    private void advance()  {
+        char ch = peek();
+        if (ch != '\0') {
+            current++;
+        }
+    }
+
+    public char nextToken () {
+        char ch = peek();
+
+        if (Character.isDigit(ch)) {
+            advance();
+            return ch;
+        }
+
+        switch (ch) {
+            case '+':
+            case '-':
+                advance();
+                return ch;
+            default:
+                break;
+        }
+
+        return '\0';
+    }
+
 }
